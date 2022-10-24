04391b4ce3c98d8ae588967163dab528ed6125be
RaThida <100819767+RaThida@users.noreply.github.com>
2022-10-24 11:58:40 +0700

    server

---
+++ connection.php
@@ -0,0 +1,6 @@
+<?php 
+   
+    //date_default_timezone_set('Asia/Phnom_Penh');
+    $conn = mysqli_connect("localhost","root","","shop_project_db") 
+    or die("couldn't connect to database"); //Database Connection
+?> 
